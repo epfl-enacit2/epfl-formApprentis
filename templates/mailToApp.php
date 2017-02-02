63d5f9affe811fc634079ca94e424a538d4fbb80
nicrausaz <n.crausaz99@gmail.com>
2017-02-02 13:16:45 +0100

    prod3.1

--- templates/mailToApp.php
+++ templates/mailToApp.php
@@ -1,7 +1,7 @@
 <?php
 $subject = 'Votre candidature pour une place d\'apprentissage';
 
-$message =  "Nous venons d'enregistrer votre candidature et vous remercions de votre intérêt pour la "."\n".
+$message = "Nous venons d'enregistrer votre candidature et vous remercions de votre intérêt pour la "."\n".
 "formation professionnelle à l’Ecole polytechnique fédérale de Lausanne."."\n\n".
 "Nous allons étudier votre dossier avec la plus grande attention et nous ne manquerons "."\n".
 "pas de vous contacter si votre profil répond à nos attentes."."\n\n";
